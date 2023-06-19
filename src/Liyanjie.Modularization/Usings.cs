b8c8e82328d9aecba1146f95da03a8684733453b
李發財 <liyanjie8712@outlook.com>
2023-06-19 18:45:50 +0800

    +

--- src/Liyanjie.Modularization/Usings.cs
+++ src/Liyanjie.Modularization/Usings.cs
@@ -3,5 +3,5 @@ global using System.Collections.Generic;
 
 global using Liyanjie.Modularization;
 
-global using Microsoft.AspNetCore.Routing;
 global using Microsoft.Extensions.DependencyInjection;
+
